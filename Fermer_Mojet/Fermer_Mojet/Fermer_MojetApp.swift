31bb86ae5c954c579922ad26e97f7c8dc2028edd
Andrey <micalman@list.ru>
2022-12-26 20:32:31 +0300

    build 0.0.1

---
+++ Fermer_Mojet/Fermer_Mojet/Fermer_MojetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fermer_MojetApp.swift
+//  Fermer_Mojet
+//
+//  Created by Andrey Samchenko on 26.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Fermer_MojetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
